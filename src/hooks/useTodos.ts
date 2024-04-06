8577f756a33acf4074b8713e9c5a694118a9a5d3
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-04-06 19:00:14 +0300

    Add useTodo hook with todoId parameter.

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -7,6 +7,17 @@ export const useTodos = () => {
     queryFn: () => todoService.getAll(),
     select: ({ data }) => data,
     // enabled: false,
-    retry: 10,
+    retry: 5,
+  });
+};
+
+export const useTodo = () => {
+  const todoId = "1";
+  return useQuery({
+    queryKey: ["todos", todoId],
+    queryFn: ({ queryKey }) => todoService.getById(queryKey[1]),
+    select: ({ data }) => data,
+    enabled: !!todoId,
+    retry: 5,
   });
 };
